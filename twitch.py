e7de590f505a2f86d08833633143514d4260753a
ysmd <88498828+yunytuny@users.noreply.github.com>
2023-03-31 00:54:37 +0600

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -3,29 +3,30 @@ import requests
 import json
 
 # Twitch API credentials
-client_id = 'YOUR_CLIENT_ID' # paste your client id
-client_secret = 'YOUR_CLIENT_SECRET' # paste your client secret
+client_id = 'jvatpoll09jilst9abwyuv0u95j8wj' # paste your client id
+client_secret = 'zo71ij5y6kcbqbf5tlk5dnmwtcf6h8' # paste your client secret
 
 # Telegram API credentials
-bot_token = 'YOUR_BOT_TOKEN' # your bot token from telegram from @BotFather
-chat_id = 'YOUR_CHAT_ID' # your channel chat id
+bot_token = '5880682505:AAECVoh2SKZxqS8oTCqf8YIHOLSlHLJ2uaQ' # your bot token from telegram from @BotFather
+chat_id = '-1001813698918' # your channel chat id
 
 # Streamer name
-streamer = 'STREAMER_NAME' # your or your favorite streamer nickname
+streamer = 'neksjgg' # your or your favorite streamer nickname
 
 # Check if streamer is live
-token = "YOUR_OAUTH_TOKEN" # paste your oAuth token from token.py
+token = "g3h4se6z41egyko1syxfotki0yxsjv" # paste your oAuth token from token.py
 url = f'https://api.twitch.tv/helix/streams?user_login={streamer}'
 headers = {
     'Client-ID': client_id,
     'Authorization': f'Bearer {token}'
 }
-response = requests.get(url, headers=headers)
-data = json.loads(response.text)
 
 while True:
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+
     if data['data']:
-        message = f'{streamer} is online! https://www.twitch.tv/{streamer}'
+        message = f'{streamer} подрубил! https://www.twitch.tv/{streamer}'
         # Send notification to Telegram channel
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         data = {'chat_id': chat_id, 'text': message}
